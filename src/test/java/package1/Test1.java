daf8f27c6e3d26f61f4230118177e11cde2b1228
Abhishek <abhishek1june@outlook.com>
2017-12-24 21:40:42 +0530

    Comment1

---
+++ src/test/java/package1/Test1.java
@@ -0,0 +1,28 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Test1 {
+
+	WebDriver driver;
+	
+	@Test(priority=0)
+	public void start() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Successfully launched Chrome Driver");
+
+	}
+	
+	@Test(priority=1)
+	public void navigate_to_main_window(){
+		String parent_win=driver.getWindowHandle();
+		driver.switchTo().window(parent_win);
+		System.out.println("Switched to main window");
+	}
+
+}
